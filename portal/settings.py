6b3a02459d61d25021f17b1d7c26150c1db03b45
spencerlevinson11 <160965421+spencerlevinson11@users.noreply.github.com>
2026-05-03 13:02:50 -0500

    Add NaberThemeMiddleware to middleware settings

--- portal/settings.py
+++ portal/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'core.theme_middleware.NaberThemeMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
